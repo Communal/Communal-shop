d244c3d94de19fb29ebe20c2fac2480015d2e2c7
sagites <chizzyikemba@gmail.com>
2025-08-08 23:40:31 +0100

    database config

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+// config/db.js
+import mongoose from 'mongoose';
+
+export default async function connectDB() {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB connected');
+  } catch (error) {
+    console.error('❌ MongoDB connection error:', error);
+    process.exit(1);
+  }
+}
