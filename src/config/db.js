149c860c69f050fadc72f38e772fa1321f37ef4d
sagites <chizzyikemba@gmail.com>
2025-08-09 22:03:49 +0100

    db config file

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,46 @@
-// config/db.js
+// // import dotenv from 'dotenv';
+// // dotenv.config();
+// import mongoose from 'mongoose';
+
+// const DATABASE_URL = process.env.DATABASE_URL;
+
+// if (!DATABASE_URL) throw new Error("Please define DATABASE_URL");
+
+// let isConnected = false;
+
+// export default async function connectDB() {
+//   if (isConnected) return;
+//   const db = await mongoose.connect(MONGODB_URI);
+//   isConnected = db.connections[0].readyState;
+// }
+
+
+// lib/mongodb.js
 import mongoose from 'mongoose';
 
+const MONGODB_URI = process.env.DATABASE_URL;
+
+ console.log(MONGODB_URI);
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the DATABASE_URL environment variable inside .env");
+}
+
+let cached = global._mongoose;
+
+if (!cached) {
+  cached = global._mongoose = { conn: null, promise: null };
+}
+
 export default async function connectDB() {
-  if (mongoose.connection.readyState >= 1) {
-    return;
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    }).then((mongoose) => mongoose);
   }
 
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('✅ MongoDB connected');
-  } catch (error) {
-    console.error('❌ MongoDB connection error:', error);
-    process.exit(1);
-  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
