9d700f07bc527b5a33f2cf1fd1ea4aa6457a254a
sagites <chizzyikemba@gmail.com>
2025-08-12 22:31:39 +0100

    data to test

---
+++ src/data/categories.js
@@ -0,0 +1,53 @@
+// Central data file for all lists (services, products, etc.)
+
+export const data = [
+  {
+    id: 'male-usa-facebook-dating',
+    name: 'Male USA Facebook Dating (location)',
+    years: { from: 2009, to: 2022 },
+    price: 17,
+  },
+  {
+    id: 'female-usa-facebook-dating',
+    name: 'Female USA Facebook Dating (location)',
+    years: { from: 2009, to: 2022 },
+    price: 17,
+  },
+  {
+    id: 'random-facebook-mostly-asian',
+    name: 'Random Facebook mostly Asian',
+    price: 5.5,
+  },
+  {
+    id: 'normal-usa-facebook-non-dating',
+    name: 'Normal USA Facebook Non Dating',
+    price: 9,
+  },
+  {
+    id: 'code-6-usa-facebook-non-dating',
+    name: 'Code 6 USA Facebook Non Dating',
+    price: 10.5,
+  },
+  {
+    id: 'code-7-usa-facebook-non-dating',
+    name: 'Code 7 USA Facebook Non Dating',
+    price: 15,
+  },
+  {
+    id: 'code-8-usa-facebook-non-dating',
+    name: 'Code 8 USA Facebook Non Dating',
+    price: 30,
+  },
+  {
+    id: 'europe-facebook',
+    name: 'Europe Facebook',
+    description:
+      'Consists of major European countries, 75% can create another profile',
+    price: 6,
+  },
+  {
+    id: 'old-nigeria-facebook',
+    name: 'Old Nigeria Facebook',
+    price: 5,
+  },
+];
