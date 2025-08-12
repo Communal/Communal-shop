46f1eb8feddeaf5a1cb4bcfb9133990ba1be62fb
sagites <chizzyikemba@gmail.com>
2025-08-12 22:30:11 +0100

    schema

---
+++ src/db/schema/Product.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  info: { type: String, maxlength: 500 },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
+  data: { type: String },
+  isSold: { type: Boolean, default: false },
+});
+
+export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
