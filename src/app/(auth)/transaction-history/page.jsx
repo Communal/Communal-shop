bd74ea8e77aa6af35e5ab65783a82e3fcbae8179
sagites <chizzyikemba@gmail.com>
2025-08-14 01:42:57 +0100

    transaction page not done

---
+++ src/app/(auth)/transaction-history/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BackHome from "@/components/Home";
+
+const TransactionHistory = () => {
+  return (
+    <main className="w-full flex flex-col gap-3">
+        <BackHome/>
+      <div>TransactionHistory</div>
+    </main>
+  );
+};
+
+export default TransactionHistory;
