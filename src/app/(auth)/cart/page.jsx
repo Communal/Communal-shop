9f1ef8b642953ad61690696aa01d816598ec3469
sagites <chizzyikemba@gmail.com>
2025-08-15 06:44:44 +0100

    cart again

--- src/app/(auth)/cart/page.jsx
+++ src/app/(auth)/cart/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import CartSummary from "@/components/CartSummary";
+import CartCard from "@/components/CartCard";
 import { useCartStore } from "@/store/cart";
 
 export default function CartPage() {
@@ -9,31 +10,19 @@ export default function CartPage() {
   const proceedToCheckout = () => {};
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">🛒 Your Cart</h1>
+    <div className="p-2">
+      <h1 className="text-2xl font-bold mb-4">Your Cart</h1>
 
       {items.length === 0 ? (
         <p className="text-gray-500">Your cart is empty.</p>
       ) : (
         <div className="space-y-4">
           {items.map((item) => (
-            <div
+            <CartCard
               key={item._id}
-              className="flex justify-between items-center border-b pb-2"
-            >
-              <div>
-                <h2 className="font-semibold">{item.name}</h2>
-                <p>
-                  ₦{item.price.toLocaleString()} × {item.quantity}
-                </p>
-              </div>
-              <button
-                onClick={() => removeFromCart(item._id)}
-                className="text-red-500 hover:underline"
-              >
-                Remove
-              </button>
-            </div>
+              item={item}
+              removeItem={() => removeFromCart(item._id)}
+            />
           ))}
 
           {/* Summary Component */}
